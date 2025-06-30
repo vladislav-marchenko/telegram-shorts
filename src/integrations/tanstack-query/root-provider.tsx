23ad03ee33acddef86f64ff795ecc748c17c335e
Владислав <maksimsraud@gmail.com>
2025-06-30 23:09:10 +0300

    fix: stale time for query client

--- src/integrations/tanstack-query/root-provider.tsx
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      //staleTime: 1000 * 60 * 5
+      staleTime: 1000 * 60 * 5
     }
   }
 })
