035f94ca6627159b94f018e9c8c9cd77e79c2aca
Владислав <maksimsraud@gmail.com>
2025-07-01 14:24:29 +0300

    fix: profile skeleton layout

--- src/components/Account/AccountInfoSkeleton.tsx
+++ src/components/Account/AccountInfoSkeleton.tsx
@@ -3,8 +3,8 @@ export const AccountInfoSkeleton = () => {
     <>
       <div className='h-40 w-40 animate-pulse rounded-full bg-neutral-600' />
       <div className='flex flex-col items-center gap-2'>
-        <div className='h-6 w-32 animate-pulse rounded-md bg-neutral-600' />
-        <div className='h-4 w-20 animate-pulse rounded-md bg-neutral-600' />
+        <div className='h-7 w-44 animate-pulse rounded-md bg-neutral-600' />
+        <div className='h-5 w-24 animate-pulse rounded-md bg-neutral-600' />
       </div>
     </>
   )
