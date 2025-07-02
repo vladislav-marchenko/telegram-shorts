8e3f2e3d968bbf25967afd023166b29924085f61
Владислав <maksimsraud@gmail.com>
2025-07-03 01:58:11 +0300

    refactor: volume icon

--- src/components/Video/VideoVolumeIcon.tsx
+++ src/components/Video/VideoVolumeIcon.tsx
@@ -21,7 +21,7 @@ export const VideoVolumeIcon = () => {
       <g
         className={cn(
           'transition-opacity duration-300 ease-in',
-          isMuted && 'opacity-80'
+          isMuted && 'opacity-85'
         )}
       >
         <path d='M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z'></path>
@@ -39,16 +39,16 @@ export const VideoVolumeIcon = () => {
             volume < 0.7 && 'opacity-0'
           )}
         />
+        <path
+          d='m2 2 20 20'
+          className='transition-[stroke-dashoffset,_stroke-opacity] duration-300 ease-in'
+          style={{
+            strokeDashoffset: isMuted ? 0 : 30,
+            strokeDasharray: 30,
+            strokeOpacity: isMuted ? 1 : 0
+          }}
+        />
       </g>
-      <path
-        d='m2 2 20 20'
-        className='transition-[stroke-dashoffset,_stroke-opacity] duration-300 ease-in'
-        style={{
-          strokeDashoffset: isMuted ? 0 : 30,
-          strokeDasharray: 30,
-          strokeOpacity: isMuted ? 1 : 0
-        }}
-      ></path>
     </svg>
   )
 }
