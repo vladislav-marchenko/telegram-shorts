ae071ba5ce8deecfbde64747bb80e35a548078d1
Владислав <maksimsraud@gmail.com>
2025-07-02 23:44:24 +0300

    fix: removed non-existent import

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,7 +1,6 @@
 import { VideoOverlay } from './VideoOverlay'
 import { VideoContext } from '@/contexts/VideoContext'
 import { VolumeContext } from '@/contexts/VolumeContext'
-import { useDefaultVolume } from '@/hooks/useDefaultVolume'
 import type { Video as VideoType } from '@/types/api'
 import type { VideoValues, VolumeValues } from '@/types/contexts'
 import { useContext, type FC } from 'react'
@@ -13,8 +12,6 @@ interface VideoProps extends VideoType {
 export const Video: FC<VideoProps> = ({ isCurrent, ...props }) => {
   const { ref, toggle } = useContext(VideoContext) as VideoValues
   const { isMuted } = useContext(VolumeContext) as VolumeValues
-
-  useDefaultVolume(ref)
 
   return (
     <div className='relative h-full w-max bg-neutral-800 max-[520px]:w-full'>
