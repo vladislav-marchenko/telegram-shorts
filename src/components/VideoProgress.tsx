7b106be58b57781ff49539fe40771c8b40cb20fa
Владислав <maksimsraud@gmail.com>
2025-07-02 18:00:06 +0300

    fix: progress bar positioning

--- src/components/VideoProgress.tsx
+++ src/components/VideoProgress.tsx
@@ -6,7 +6,7 @@ export const VideoProgress = () => {
   const { progress } = useContext(VideoContext) as VideoValues
 
   return (
-    <div className='absolute bottom-[50px] h-[3px] w-full bg-neutral-600/50'>
+    <div className='absolute bottom-[54px] h-[3px] w-full bg-neutral-600/50'>
       <div
         className='absolute top-0 left-0 h-full w-full bg-neutral-100/70'
         style={{ width: `${progress}%` }}
