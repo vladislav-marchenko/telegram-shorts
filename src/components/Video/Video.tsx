ce30ce324a492dc0ca4e37ad39ace96e6a88b42a
Владислав <maksimsraud@gmail.com>
2025-07-03 22:47:57 +0300

    feat: added video scaling based on ratio

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,6 +1,7 @@
 import { VideoOverlay } from './VideoOverlay'
 import { VideoContext } from '@/contexts/VideoContext'
 import { VolumeContext } from '@/contexts/VolumeContext'
+import { cn } from '@/lib/utils'
 import type { Video as VideoType } from '@/types/api'
 import type { VideoValues, VolumeValues } from '@/types/contexts'
 import { useContext, type FC } from 'react'
@@ -13,13 +14,26 @@ export const Video: FC<VideoProps> = ({ isCurrent, ...props }) => {
   const { ref, toggle } = useContext(VideoContext) as VideoValues
   const { isMuted } = useContext(VolumeContext) as VolumeValues
 
+  const video = ref.current
+  const width = video?.clientWidth ?? 0
+  const height = video?.clientHeight ?? 0
+  const ratio = width / height
+
   return (
-    <div className='relative h-full w-max bg-neutral-800 max-[520px]:w-full'>
+    <div
+      className={cn('relative h-full w-max bg-neutral-800', {
+        'max-[1200px]:w-full': ratio > 1,
+        'max-[720px]:w-full': ratio < 1
+      })}
+    >
       <video
         ref={ref}
         src={props.url}
         onClick={toggle}
-        className='h-dvh w-full max-[520px]:object-cover'
+        className={cn('h-dvh w-full', {
+          'max-[1200px]:object-cover': ratio > 1,
+          'max-[720px]:object-cover': ratio < 1
+        })}
         playsInline
         autoPlay={isCurrent}
         controls={false}
