4100cafe3bdf9816b1e30fb3d12c3d2019f8122a
Владислав <maksimsraud@gmail.com>
2025-07-03 15:09:58 +0300

    feat: added progress bar disappearing

--- src/components/Video/VideoProgress.tsx
+++ src/components/Video/VideoProgress.tsx
@@ -6,10 +6,10 @@ export const VideoProgress = () => {
   const { progress } = useContext(VideoContext) as VideoValues
 
   return (
-    <div className='absolute bottom-16 w-full px-4 md:bottom-20'>
-      <div className='relative h-1 w-full overflow-hidden rounded-full bg-neutral-600/50'>
+    <div className='absolute w-full p-6 opacity-0 transition-opacity hover:opacity-100 md:bottom-14'>
+      <div className='group relative h-1.5 w-full cursor-pointer overflow-hidden rounded-full bg-neutral-600/50 transition-all hover:h-2 hover:bg-neutral-600'>
         <div
-          className='absolute top-0 left-0 h-full w-full bg-neutral-200/70'
+          className='absolute top-0 left-0 h-full w-full bg-neutral-200/70 transition-colors group-hover:bg-neutral-200'
           style={{ width: `${progress}%` }}
         />
       </div>
