0f6bf7e068b94340c90d789cfa3b10ad473180d0
Владислав <maksimsraud@gmail.com>
2025-07-04 17:20:02 +0300

    feat: changed volume icon

--- src/components/Video/VideoVolumeIcon.tsx
+++ src/components/Video/VideoVolumeIcon.tsx
@@ -18,37 +18,36 @@ export const VideoVolumeIcon = () => {
       stroke-linecap='round'
       stroke-linejoin='round'
     >
-      <g
+      <path
+        d='M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z'
         className={cn(
           'transition-opacity duration-300 ease-in',
-          isMuted && 'opacity-85'
+          volume === 0 && 'opacity-70'
         )}
-      >
-        <path d='M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z'></path>
-        <path
-          d='M16 9a5 5 0 0 1 0 6'
-          className={cn(
-            'transition-opacity duration-300 ease-in',
-            volume < 0.3 && 'opacity-0'
-          )}
-        />
-        <path
-          d='M19.364 18.364a9 9 0 0 0 0-12.728'
-          className={cn(
-            'transition-opacity duration-300 ease-in',
-            volume < 0.7 && 'opacity-0'
-          )}
-        />
-        <path
-          d='m2 2 20 20'
-          className='transition-[stroke-dashoffset,_stroke-opacity] duration-300 ease-in'
-          style={{
-            strokeDashoffset: isMuted ? 0 : 30,
-            strokeDasharray: 30,
-            strokeOpacity: isMuted ? 1 : 0
-          }}
-        />
-      </g>
+      />
+      <path
+        d='M16 9a5 5 0 0 1 0 6'
+        className={cn(
+          'transition-opacity duration-300 ease-in',
+          volume < 0.3 && 'opacity-70'
+        )}
+      />
+      <path
+        d='M19.364 18.364a9 9 0 0 0 0-12.728'
+        className={cn(
+          'transition-opacity duration-300 ease-in',
+          volume < 0.7 && 'opacity-70'
+        )}
+      />
+      <path
+        d='m2 2 20 20'
+        className='transition-[stroke-dashoffset,_stroke-opacity] duration-300 ease-in'
+        style={{
+          strokeDashoffset: isMuted ? 0 : 30,
+          strokeDasharray: 30,
+          strokeOpacity: isMuted ? 1 : 0
+        }}
+      />
     </svg>
   )
 }
