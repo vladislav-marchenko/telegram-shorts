b2de6c9b27817e2a51ac52b7808eaf1e8d616e32
Владислав <maksimsraud@gmail.com>
2025-07-05 12:38:30 +0300

    feat: changed volume icon

--- src/components/Video/VideoVolumeIcon.tsx
+++ src/components/Video/VideoVolumeIcon.tsx
@@ -22,21 +22,21 @@ export const VideoVolumeIcon = () => {
         d='M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z'
         className={cn(
           'transition-opacity duration-300 ease-in',
-          volume === 0 && 'opacity-70'
+          volume === 0 && 'opacity-65'
         )}
       />
       <path
         d='M16 9a5 5 0 0 1 0 6'
         className={cn(
           'transition-opacity duration-300 ease-in',
-          volume < 0.3 && 'opacity-70'
+          volume < 0.3 && 'opacity-0'
         )}
       />
       <path
         d='M19.364 18.364a9 9 0 0 0 0-12.728'
         className={cn(
           'transition-opacity duration-300 ease-in',
-          volume < 0.7 && 'opacity-70'
+          volume < 0.7 && 'opacity-0'
         )}
       />
       <path
