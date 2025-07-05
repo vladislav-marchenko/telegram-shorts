e81138c48153a2167a554e1db00d4a5b8aa74ccf
Владислав <maksimsraud@gmail.com>
2025-07-05 11:15:19 +0300

    refactor: removed unnecessary memo

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,19 +1,21 @@
 import { VideoContent } from './VideoContent'
 import { VideoContextProvider } from '@/contexts/VideoContext'
 import type { Video as VideoType } from '@/types/api'
-import { memo, type FC } from 'react'
+import { type FC } from 'react'
 
 interface VideoProps extends VideoType {
   isCurrent?: boolean
   isVisible: boolean
 }
 
-export const Video: FC<VideoProps> = memo(
-  ({ isCurrent = true, isVisible, ...props }) => {
-    return (
-      <VideoContextProvider isVisible={isVisible}>
-        <VideoContent {...props} isCurrent={isCurrent} />
-      </VideoContextProvider>
-    )
-  }
-)
+export const Video: FC<VideoProps> = ({
+  isCurrent = true,
+  isVisible,
+  ...props
+}) => {
+  return (
+    <VideoContextProvider isVisible={isVisible}>
+      <VideoContent {...props} isCurrent={isCurrent} />
+    </VideoContextProvider>
+  )
+}
